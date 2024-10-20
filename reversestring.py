31f641384b808649f03d54203d09d58952a7da7a
ItsMeMahiG <mahi.gadicherlaboba@gmail.com>
2024-10-20 12:04:33 +0530

    mahi is the best at for loop

---
+++ reversestring.py
@@ -0,0 +1,7 @@
+s=input("please enter a string : ")
+s2=('')
+for i in s :
+    s2=i+s2
+
+print("\n original string=",s)
+print("revesed string=",s2)
---
+++ sumwholeno.py
@@ -0,0 +1,6 @@
+num=int(input("enter the term to which u want the sum : "))
+sum=0
+for i in range(0,num + 1) :
+    sum= sum+i
+    print("\n i=",i)
+    print(" sum=",sum)
